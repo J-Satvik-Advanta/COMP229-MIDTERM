da62f6b60c270b42d2d06bdf8c81ca23a755a6e9
Satvik Kumar Jain <satvik.jain@advantawitty.com>
2024-06-18 20:46:34 -0400

    Changing file name for home page

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Home: React.FC = () => {
   return (
     <div className="container-fluid mt-5 text-center">
-      <h1 className="mb-4">Home Page</h1>
+      <h1 className="mb-5">Home Page</h1>
       <div className="d-flex justify-content-center">
         <Link to="/add-product" className="btn btn-primary me-2">Add Product</Link>
         <Link to="/sign-up" className="btn btn-secondary">Sign Up User</Link>
