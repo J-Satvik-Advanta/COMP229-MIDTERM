075bc7453784c7864fb6d6c37b472f1bd7926c8e
Satvik Kumar Jain <satvik.jain@advantawitty.com>
2024-06-18 20:13:12 -0400

    Updating the Midterm Project 1) Adding seperate Home Page 2) Removing unnecessary styling 3) Updating App.tsx to handle routes correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 import AddProduct from './pages/AddProduct';
 import SignUpUser from './pages/SignUpUser';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <div className="container-fluid mt-5 text-center">
-        <h1 className="mb-4">Home Page</h1>
-        <div className="d-flex justify-content-center">
-          <Link to="/add-product" className="btn btn-primary me-2">Add Product</Link>
-          <Link to="/sign-up" className="btn btn-secondary">Sign Up User</Link>
-        </div>
-        <Routes>
-          <Route path="/add-product" element={<AddProduct />} />
-          <Route path="/sign-up" element={<SignUpUser />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-product" element={<AddProduct />} />
+        <Route path="/sign-up" element={<SignUpUser />} />
+      </Routes>
     </Router>
   );
 };
---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home: React.FC = () => {
+  return (
+    <div className="container-fluid mt-5 text-center">
+      <h1 className="mb-4">Home Page</h1>
+      <div className="d-flex justify-content-center">
+        <Link to="/add-product" className="btn btn-primary me-2">Add Product</Link>
+        <Link to="/sign-up" className="btn btn-secondary">Sign Up User</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
